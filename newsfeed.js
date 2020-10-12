ffb8cf99ec53badc2fe5ef77e32248bcb9a3c19c
neden <prog.enthusiast@gmail.com>
2020-10-12 02:20:24 -0400

    Update newsfeed.js

--- newsfeed.js
+++ newsfeed.js
@@ -1,17 +1,17 @@
 {
-    2:{
+    "2":{
         "title": "Thd",
         "date": "13/10/2020",
         "tease": "Day 3",
         "content": "Copyright (c) 2018 Copyright Holder All Rights Reserved."       
     },
-    1:{
+    "1":{
         "title": "Sec",
         "date": "12/10/2020",
         "tease": "Day 2",
         "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."       
     },
-    0:{
+    "0":{
         "title": "Introduction",
         "date": "11/10/2020",
         "tease": "Day 1 - Stay tuned for more news and announcements!",
