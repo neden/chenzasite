976a14133f0d54e9c885fa83be507670bbb1089a
neden <prog.enthusiast@gmail.com>
2020-10-11 23:01:24 -0400

    Update newsfeed.js

--- newsfeed.js
+++ newsfeed.js
@@ -9,7 +9,7 @@
         "title": "Sec",
         "date": "12/10/2020",
         "tease": "Day 2",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."       
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."       
     },
     0:{
         "title": "Introduction",
