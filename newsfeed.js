33128e7ba0d8245af7139414a9e184c0f0180a82
neden <prog.enthusiast@gmail.com>
2021-02-15 09:09:14 -0500

    Update newsfeed.js

--- newsfeed.js
+++ newsfeed.js
@@ -1,26 +1,26 @@
 {
     "3":{
         "title": "Game Announcement",
-        "date": "16/10/2021",
+        "date": "24/02/2021",
         "tease": "Word Flair is out now!",
-        "content": "<div class='fullcont'><iframe src='https://www.youtube.com/embed/opZaeDvVRwI' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe><br><font>Word Flair Official Trailer</font></div>"       
+        "content": "<div class='fullcont'><iframe width='100%' height='100%' src='https://www.youtube.com/embed/opZaeDvVRwI' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe><br><font>Word Flair Official Trailer</font></div>"
     },
     "2":{
         "title": "Game Announcement",
         "date": "15/01/2020",
-        "tease": "Release Date Announced!",
-        "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/chenzasite@master/images/wordflair_bg_advert_bg_socmed2.png' style='object-fit: cover;' width='100%' height='500px'><br><font>Site Updates</font></div>"       
+        "tease": "24/02/2021",
+        "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/chenzasite@master/images/wordflair_bg_advert_bg_socmed2.png' style='object-fit: cover;' width='100%' height='100%'><br><font></font></div>"
     },
     "1":{
         "title": "Company Announcement",
-        "date": "12/10/2020",
+        "date": "12/11/2020",
         "tease": "Upcoming Game Announcement",
-        "content": "Stay tuned for our latest game, Word Flair - A fun and clever puzzle game coming soon for Android and iOS."       
+        "content": "Stay tuned for our latest game, Word Flair - A fun and clever puzzle game coming soon for Android and iOS."
     },
     "0":{
         "title": "Chenza Studios - Day 1",
         "date": "11/10/2020",
-        "tease": "We are a mobile-game company based. We are here to deliver you fun and casual gaming. Stay tuned for our more news on our upcoming games!",
-        "content": true      
+        "tease": "Hello World!",
+        "content": "We are a mobile-game company based. We are here to deliver you fun and casual gaming. Stay tuned for our more news on our upcoming games!"
     }
 }
