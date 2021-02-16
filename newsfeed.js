0a422b580fd569eefdd228e6d7cf10e20cb6a797
neden <prog.enthusiast@gmail.com>
2021-02-16 09:57:47 -0500

    Update newsfeed.js

--- newsfeed.js
+++ newsfeed.js
@@ -3,7 +3,7 @@
         "title": "Game Announcement",
         "date": "24/02/2021",
         "tease": "Word Flair is out now!",
-        "content": "<div class='fullcont'><iframe width='100%' height='600px' src='https://www.youtube.com/embed/opZaeDvVRwI' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe><br><font>Word Flair Official Trailer</font></div>"
+        "content": "<div class='fullcont'><iframe width='100%' height='600px' src='https://www.youtube.com/embed/opZaeDvVRwI' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe><br><font>Word Flair Official Trailer!</font></div>"
     },
     "2":{
         "title": "Game Announcement",
@@ -12,15 +12,15 @@
         "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/chenzasite@master/images/wordflair_bg_advert_bg_socmed3.png' style='object-fit: cover;' width='100%' height='100%'><br><font></font></div>"
     },
     "1":{
-        "title": "Company Announcement",
+        "title": "Game Announcement",
         "date": "12/11/2020",
-        "tease": "Upcoming Game Announcement",            
-        "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/wordflairres@master/img/wordflair_font.png' style='object-fit: cover;' width='25%' height='100%'><br><font>Upcoming Project: Word Flair - A fun and clever puzzle game coming soon for Android and iOS.</font></div>"
+        "tease": "Word Flair!",            
+        "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/wordflairres@master/img/wordflair_font.png' style='object-fit: cover;' width='25%' height='100%'><br><font>Latest Project: Word Flair - A fun, casual and clever word game to be released on Android and iOS.</font></div>"
     },
     "0":{
         "title": "Greetings",
         "date": "11/10/2020",
         "tease": "Day 1",
-        "content": "We are a mobile-game company based. We are here to deliver you fun and casual gaming. Stay tuned for our more news on our upcoming games!"
+        "content": "Chenza Studios is a mobile-based game company. We are here to deliver you fun and casual gaming. Stay tuned for our more news on our upcoming games!"
     }
 }
