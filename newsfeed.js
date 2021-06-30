115507756668f7f7ff831bfd7253f7ff500ddb01
neden <prog.enthusiast@gmail.com>
2021-06-29 23:27:05 -0400

    Update newsfeed.js

--- newsfeed.js
+++ newsfeed.js
@@ -1,21 +1,21 @@
 {
     "3":{
         "title": "Game Announcement",
-        "date": "24/02/2021",
+        "date": "02/07/2021",
         "tease": "Word Flair Official Trailer - Game is out now!",
-        "content": "<div class='fullcont'><iframe width='100%' height='600px' src='https://www.youtube.com/embed/opZaeDvVRwI' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe><br><font>Word Flair Official Trailer!</font></div>"
+        "content": "<div class='fullcont'><iframe width='100%' height='600px' src='https://www.youtube.com/embed/ki5qKuHtFIs' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe><br><font>Word Flair - Official Trailer!</font></div>"
     },
     "2":{
         "title": "Game Announcement",
-        "date": "15/01/2020",
-        "tease": "Out 02.24.2021",
+        "date": "15/01/2021",
+        "tease": "Out 02.07.2021",
         "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/chenzasite/images/wordflair_bg_advert_bg_socmed3.png' style='object-fit: cover;' width='100%' height='100%'><br><font></font></div>"
     },
     "1":{
         "title": "Game Announcement",
         "date": "12/11/2020",
         "tease": "Exciting Reveal!",            
-        "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/wordflairres/img/wordflair_font.png' style='object-fit: cover;' width='25%' height='100%'><br><font>Latest Project: Word Flair - A fun, casual and clever word game to be released on Android and iOS.</font></div>"
+        "content": "<div class='fullcont'><img src='https://cdn.jsdelivr.net/gh/neden/wordflairres/img/wordflair_font.png' style='object-fit: cover;' width='25%' height='100%'><br><font>Word Flair - A fun, casual and clever word game to be released on Android and iOS.</font></div>"
     },
     "0":{
         "title": "Greetings",
